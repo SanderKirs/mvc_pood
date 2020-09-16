b9faf17a8d7db4939a05f7f42a38c87550b29626
Sander Kirs <sander.kirs@khk.ee>
2020-09-16 11:17:37 +0300

    Created and tested shop description

--- ui.py
+++ ui.py
@@ -1,13 +1,17 @@
 #ui.py
 
 from product import Product
+from shop import Shop
 
 #creating products
 bread = Product("Bread", 0.80, 10)
 milk = Product("Milk", 0.50, 50)
 wine = Product("Wine", 5.60, 5)
 
-#Testing viewing the product
-print(bread)
-print(milk)
-print(wine)
+#Creating the shop and adding the products to it
+shop = Shop()
+shop.addProduct(bread)
+shop.addProduct(milk)
+shop.addProduct(wine)
+
+print(shop)
